2ddd6c11d842ce4fd34f398aa825ae1964a214a1
jeferson.guesser <jeferson.guesser@lab1308.catolicasc.org.br>
2018-10-23 22:30:11 -0200

    Realizada atividade avaliativa.

---
+++ src/banco/modelo/Livro.java
@@ -0,0 +1,74 @@
+package banco.modelo;
+
+public class Livro implements Imprimivel {
+
+	private int id;
+	private String titulo;
+	private int anoPublicacao;
+	private String editora;
+	private Autor autor;
+
+	public Livro() {}
+	
+	public Livro(int id, String titulo, int anoPublicacao, String editora, Autor autor) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.anoPublicacao = anoPublicacao;
+		this.editora = editora;
+		this.autor = autor;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public int getAnoPublicacao() {
+		return anoPublicacao;
+	}
+
+	public void setAnoPublicacao(int anoPublicacao) {
+		this.anoPublicacao = anoPublicacao;
+	}
+
+	public String getEditora() {
+		return editora;
+	}
+
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+
+	public Autor getAutor() {
+		return autor;
+	}
+
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+
+	@Override
+	public String imprimeEmLista() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] getColunas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
